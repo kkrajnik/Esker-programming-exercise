9b9235d73a8f16e11a19b68a3b09f029adad1747
kkrajnik <69048486+kkrajnik@users.noreply.github.com>
2020-07-31 11:58:50 -0500

    Update main.py
    starting analysis

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+def analyze_line(line):
+  
 
+with open("samplefile.txt", "r") as file:
+  end = false
+  while !end:
+    line = file.readline()
+    if line == "":
+      end = true
+    else:
+      analyze_line(line)
